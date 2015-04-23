5328b4864afc1cbaabfd27a6078e78eb9f209f8b
Ace Han <ace.jl.han@gmail.com>
2015-04-24 00:09:48 +0800

    first trial on django-rest-framework and jwt auth

--- www/app/auth/services/auth.service.js
+++ www/app/auth/services/auth.service.js
@@ -15,13 +15,49 @@ function (module, namespace) {
 
     function authService($http, $location, $q, $window){
         var service = {
-            login: login
+            setToken: setToken,
+            getToken: getToken,
+            deleteToken: deleteToken,
+            login: login,
+            logout: logout
         }
 
         return service;
 
+        function getToken() {
+            return $window.localStorage.getItem('token');
+        }
+
+        function setToken(token) {
+            $window.localStorage.setItem('token', token);
+        }
+
+        function deleteToken() {
+            $window.localStorage.removeItem('token');
+        }
+
         function login(username, password){
             console.log(username, password);
+            var deferred = $q.defer();
+
+            $http.post('/api/v1/account/login/', 
+                {   username: username, 
+                    password: password
+                }).success(function (response, status, headers, config) {
+                    if (response.token) {
+                        service.setToken(response.token);
+                    }
+
+                    deferred.resolve(response, status, headers, config);
+                }).error(function (response, status, headers, config) {
+                    deferred.reject(response, status, headers, config);
+                });
+
+            return deferred.promise;
+        }
+
+        function logout() {
+          service.deleteToken();
         }
     }
 });
