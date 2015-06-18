ca96245e6cd0226f61792f924907246e8a6aa6c2
Ace Han <ace.jl.han@gmail.com>
2015-06-18 12:14:36 +0800

    ref #4, add ionTabs inside home ionView(not ionVontent)

--- www/app/routes.js
+++ www/app/routes.js
@@ -25,7 +25,7 @@ function (appModule, namespace) {
                     // every submodule's root may define like this
                     '@': {
                       // it's bad solution doing this
-                      templateUrl: "app/group/templates/index.html"
+                      templateUrl: "templates/home.html"
                     }
                   }
                 })
