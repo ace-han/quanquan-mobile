9a9d85a52b4fb2868095b2e98cf7aa7b84f5ec0f
Peng Yue <pypypypypy@msn.cn>
2015-09-12 16:12:52 +0800

    read contacts with cordova-plugin-contacts

--- www/app/contact/controllers/contacts.controller.js
+++ www/app/contact/controllers/contacts.controller.js
@@ -11,21 +11,39 @@ function (module, namespace) {
                 
     ContactsController.$inject = ['$scope', '$timeout'];
 
-    console.log('enter contact controller');
     return ContactsController;
 
     function ContactsController($scope, $timeout) {
         var vm = this;
 
         vm.contacts = [];
-        console.log('enter contact controller2');
-        for(var i=0; i<3; i++){
-            console.log('push contacts');
-            vm.contacts.push({slug: i
-                        , imgSrc: 'http://ionicframework.com/img/docs/venkman.jpg'
-                        , name: ' New Item ' + Math.floor(Math.random() * 1000) + 4});
-        }
         
+        if (navigator == null || navigator == undefined || navigator.contacts == undefined)
+        {
+            for(var i=0; i<3; i++){
+                console.log('push contacts');
+                vm.contacts.push({slug: i
+                            , imgSrc: 'http://ionicframework.com/img/docs/venkman.jpg'
+                            , name: ' New Item ' + Math.floor(Math.random() * 1000) + 4});
+            }
+        }
+        else
+        {
+            console.log("start to read contacts");
+            var fields = ["displayName", "name", "phoneNumbers"];
+            navigator.contacts.find(fields, OnReadContactsSucc);
+        }
+                                    
+        function OnReadContactsSucc(contacts)
+        {
+            console.log("read contacts successfully");
+            for(var i = 0; i < contacts.length; i++){
+                vm.contacts.push({slug: i
+                            , imgSrc: 'http://ionicframework.com/img/docs/venkman.jpg'
+                            , name: contacts[i].name.givenName
+                            , phone: contacts[i].phoneNumbers[0].value});
+            }
+        }
     }
 
 });
