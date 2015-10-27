01629f9a78392ce23b1c91d866fbadc07659a6c3
Ace Han <ace.jl.han@gmail.com>
2015-10-27 22:22:13 +0800

    update qa env BaseUrl

--- www/app/app.js
+++ www/app/app.js
@@ -57,11 +57,13 @@ function (angular, namespace
             // config global Restangular
             if(window.cordova){
                 // for simplicity, when cordova is available
-		if(window.cordova.env == 'qa'){
-			RestangularProvider.setBaseUrl('http://onedegree-qa.madeinace.com/api/v1');
-		} else {
+                // RestangularProvider.setBaseUrl('http://192.168.56.1:8090/api/v1');
+                if(window.cordova.env == 'qa'){
+                    RestangularProvider.setBaseUrl('http://onedegree-qa.madeinace.com/api/v1');
+                } else {
                 	RestangularProvider.setBaseUrl('http://onedegree.madeinace.com/api/v1');   
                 }
+        
             } else {
                 RestangularProvider.setBaseUrl('/api/v1');
             }
