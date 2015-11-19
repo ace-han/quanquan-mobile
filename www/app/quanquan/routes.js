0ce13b6f16df3bbc8d328bbc810dad3ca1f20c18
Ace <ace.jl.han@gmail.com>
2015-11-19 22:08:37 +0800

    simplify index page

--- www/app/quanquan/routes.js
+++ www/app/quanquan/routes.js
@@ -5,95 +5,109 @@ define([
 ],
 function (quanquanModule, moduleNamespace, appNamespace) {
     'use strict';
-    return quanquanModule.config([
-        '$stateProvider'
-        , '$urlRouterProvider'
-        , '$futureStateProvider'
-        , function($stateProvider, $urlRouterProvider, $futureStateProvider){
-          $stateProvider
-            // it's necessary to declare this abstract one for state 'app' in app/routes.js have its direct children initialized!!!
-            .state(moduleNamespace, { 
-              url: ''  // it's very important that with an empty string here to make it home page
-              , abstract: true
-              , parent: appNamespace
-            })
-            .state(moduleNamespace + '.index', { 
-              url: ''
-              , views: {
-                // with this '@' means unname view at root level
-                // every submodule's root may define like this
-                '@': {
-                  // it's bad solution doing this
-                  templateUrl: 'app/quanquan/templates/index.html'
-                  // but the controller still could take resolved data
-                  , controller: moduleNamespace + '.ChannelsController as channelsController'
-                }
-              }
-              // please be aware of using below on state level not view level
-              // http://stackoverflow.com/questions/27719183/onenter-and-onexit-are-not-called-when-state-is-left-activated-in-angularjs-ui-r#answer-27721831
-              // no necessary for the resolve section
-              // , resolve: {
-              //     isLoading: ['channelService']
-              // }
-              , onEnter: ['$state', '$ionicHistory', '$ionicLoading', moduleNamespace + '.channelService', 
-                      function ($state, $ionicHistory, $ionicLoading, channelService){
-                channelService.getChannelStateSettings().then(
-                    function success(states){
-                        var firstState = states[0];
-                        console.info('resolved 1st channel:', firstState.name);
-                        $state.go(firstState.name);
-                        // console.info($ionicHistory.viewHistory() );
-                    }, function error(err){
-                      $ionicLoading.show({ template: err, 
-                          noBackdrop: true, duration: 2000 });
-                    });
-              }]
-            })
-            .state(moduleNamespace + '.termsOfService', {
-              url: '/terms-of-service'
-              // cache setting I've decided placing them in routes.js
-              , cache: false
-              , views: {
-                '@': {
-                  templateUrl: 'app/quanquan/templates/terms_of_service.html'
-                  
-                }
-              }
-            })
-            .state(moduleNamespace + '.privacyPolicy', {
-              url: '/privacy-policy'
-              , cache: false
-              , views: {
-                '@': {
-                  templateUrl: 'app/quanquan/templates/privacy_policy.html'
-                  
-                }
-              }
-            })
-
-
-          $futureStateProvider.stateFactory('quanquan', generalStateFactory);
-          $futureStateProvider.addResolve(loadAndRegisterFutureStates);
-
-          // resolver just setting retrieval
-          loadAndRegisterFutureStates.$inject = [moduleNamespace + '.channelService'];
-          function loadAndRegisterFutureStates(channelService){
-            // may change to a general url service later
-            return channelService.getChannelStateSettings().then(
-                        function(stateSettings){
-                           angular.forEach(stateSettings, function (fstate) {
-                            $futureStateProvider.futureState(fstate);
-                          });
-                        }
-                    );
+    config.$inject = ['$stateProvider', '$urlRouterProvider', '$futureStateProvider'];
+    function config($stateProvider, $urlRouterProvider, $futureStateProvider){
+      $urlRouterProvider.when('', '/');
+      $stateProvider
+        // it's necessary to declare this abstract one for state 'app' in app/routes.js have its direct children initialized!!!
+        .state(moduleNamespace, { 
+          url: ''  // it's very important that with an empty string here to make it home page
+          , abstract: true
+          , parent: appNamespace
+        })
+        .state(moduleNamespace + '.index', { 
+          url: '/'
+          , cache: false
+          , views: {
+            // with this '@' means unname view at root level
+            // every submodule's root may define like this
+            '@': {
+              // it's bad solution doing this
+              templateUrl: 'app/quanquan/templates/index.html'
+              // but the controller still could take resolved data
+              , controller: moduleNamespace + '.ChannelsController as channelsController'
+            }
           }
-          // setFactory will actually do the new state register
-          generalStateFactory.$inject = ['$q', 'futureState'];
+          //, redirectTo: 'quanquan.index.recommendation'
+          // please be aware of using below on state level not view level
+          // http://stackoverflow.com/questions/27719183/onenter-and-onexit-are-not-called-when-state-is-left-activated-in-angularjs-ui-r#answer-27721831
+          // no necessary for the resolve section
+          // , resolve: {
+          //     isLoading: ['channelService']
+          // }
 
-          function generalStateFactory($q, futureState){
-            // later, we will add other url
-            // futureState's struct depends on the return value from loadAndRegisterFutureStates function
-            return $q.when(futureState);
+          // , onEnter: ['$state', '$ionicHistory', '$ionicLoading', moduleNamespace + '.channelService', 
+          //         function ($state, $ionicHistory, $ionicLoading, channelService){
+          //   channelService.getChannelStateSettings().then(
+          //       function success(states){
+          //           var firstState = states[0];
+          //           console.info('resolved 1st channel:', firstState.name);
+          //           $state.go(firstState.name);
+          //           // console.info($ionicHistory.viewHistory() );
+          //       }, function error(err){
+          //         $ionicLoading.show({ template: err, 
+          //             noBackdrop: true, duration: 2000 });
+          //       });
+          // }]
+        })
+        .state(moduleNamespace + '.termsOfService', {
+          url: '/terms-of-service'
+          // cache setting I've decided placing them in routes.js
+          , cache: false
+          , views: {
+            '@': {
+              templateUrl: 'app/quanquan/templates/terms_of_service.html'
+              
+            }
           }
-  }])
+        })
+        .state(moduleNamespace + '.privacyPolicy', {
+          url: '/privacy-policy'
+          , cache: false
+          , views: {
+            '@': {
+              templateUrl: 'app/quanquan/templates/privacy_policy.html'
+              
+            }
+          }
+        })
+
+
+      // $futureStateProvider.stateFactory('quanquan', generalStateFactory);
+      // $futureStateProvider.addResolve(loadAndRegisterFutureStates);
+
+      // // resolver just setting retrieval
+      // loadAndRegisterFutureStates.$inject = [moduleNamespace + '.channelService'];
+      // function loadAndRegisterFutureStates(channelService){
+      //   // may change to a general url service later
+      //   return channelService.getChannelStateSettings().then(
+      //               function(stateSettings){
+      //                  angular.forEach(stateSettings, function (fstate, i) {
+      //                   if(i>0){fstate.url = '^'+fstate.url}
+      //                   $futureStateProvider.futureState(fstate);
+      //                 });
+      //               }
+      //           );
+      // }
+      // // setFactory will actually do the new state register
+      // generalStateFactory.$inject = ['$q', 'futureState'];
+
+      // function generalStateFactory($q, futureState){
+      //   // later, we will add other url
+      //   // futureState's struct depends on the return value from loadAndRegisterFutureStates function
+      //   return $q.when(futureState);
+      // }
+  }
+  // run.$inject = ['$rootScope', '$state'];
+  // function run($rootScope, $state) {
+  //   $rootScope.$on('$stateChangeStart', function(evt, to, params) {
+  //     if (to.redirectTo) {
+  //       evt.preventDefault();
+  //       $state.go(to.redirectTo, params)
+  //     }
+  //   });
+  // }
+  quanquanModule.config(config);
+                //.run(run);
+  return config;
 })
